b7f866b49aa0cdc603b9b71b1f2c3e2bd991a3d2
Michelle Wan <Michelle@10-16-186-163.dynapool.wireless.nyu.edu>
2021-12-17 17:59:13 -0500

    final

--- final/script.js
+++ final/script.js
@@ -1,24 +1,53 @@
+var x = [];
+var y = [];
+var w = 33.5;
+var color = [];
+
 function setup() {
-  var = myCanvas = createCanvas(displayWidth, displayHeight);
-  myCanvas.parent("p5");
 
-	// Getting microphone input
-	mic = new p5.AudioIn();
-  mic.start();
+    const saveButton = createButton('Save Avatar');
+    saveButton.mousePressed(() => save(canvas, 'Avatar.jpg'));
 
-	// Setting up the amplitude module
-	amplitude = new p5.Amplitude(0.5);
-	amplitude.setInput(mic);
-	// Get a better range
-	amplitude.toggleNormalize(1);
+    var canvas = createCanvas(700, 700);
+    for (var i = 0; i < 20; i++) {
+        x[i] = w + i * w;
+        y[i] = w + i * w;
+    }
+
+    //color in each cell = true
+    for (var i = 0; i < 400; i++) {
+        color[i] = true;
+    }
+
 }
 
+
 function draw() {
-  background(0);
-	fill(255);
-	Measure the volume
-	volume = 10 + amplitude.getLevel()*300;
-	// print(volume)
-	ellipse(displayWidth/2, displayHeight/2, volume, volume);
-  ellipse(mouseX, mouseY, 400, 400)
+    clear();
+    rectMode(CENTER);
+    stroke(200);
+    for (var j = 0; j < y.length; j++) {
+        for (var i = 0; i < 20; i++) {
+            if (color[j * 20 + i]) {
+                fill(255);
+            } else {
+                fill(0);
+            }
+
+            rect(x[i], y[j], w, w);
+        }
+    }
+}
+
+function mousePressed() {
+    for (var i = 0; i < 20; i++) {
+        for (var j = 0; j < 20; j++) {
+            var dis = dist(mouseX, mouseY, x[i], y[j]);
+            if (dis < w/2) {
+                color[j * 20 + i] =! color[j * 20 + i];
+            }
+        }
+    }
+
+
 }
